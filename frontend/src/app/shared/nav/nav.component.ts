409b7828acaa82da3a8ab07327979db27f2d5386
EDU4RDO-DEV <joseguzman20162020@gmail.com>
2024-03-23 00:22:22 -0600

    Se agrego una comprobación de estado de sesión

--- frontend/src/app/shared/nav/nav.component.ts
+++ frontend/src/app/shared/nav/nav.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+  userLoginOn:boolean = false;
+  constructor() { }
 
+  ngOnInit(): void {
+    if(localStorage.getItem('token')){
+      this.userLoginOn = true;
+    }
+  }
 }
