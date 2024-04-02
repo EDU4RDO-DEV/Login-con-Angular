2c36408f4288a7d6c3e73baed56bbced23461caa
EDU4RDO-DEV <joseguzman20162020@gmail.com>
2024-04-01 21:43:35 -0600

    Mapeo de Rutas y estructura del formulario

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,22 @@ import { FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit{
 
+  loginForm = this.formBuilder.group({
+    // Valor por defecto, campo requerido, email valido, maximo de caracteres
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(25)]],
+    password: ['', [Validators.required, Validators.maxLength(25)]]
+  })
   constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  // Funcion para enviar los datos del formulario
+  login(){
+    if(this.loginForm.valid){
+      console.log('Llamar al servicio de login');
+    }else{
+      alert('Error al ingresar los datos.');
+    }
+  }
 }
